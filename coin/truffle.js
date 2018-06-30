a2706c7aceafb69a4888f40ddc9f8575e10a3a02
mattiaxelricci <axe.ricci@stud.uniroma3.it>
2018-06-30 19:38:12 +0200

    fixed test, still problem with truffle deployer on ropsten

--- coin/truffle.js
+++ coin/truffle.js
@@ -3,6 +3,7 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
+<<<<<<< HEAD
       network_id: "*" // Match any network id
     },
   ropsten:  {
@@ -18,5 +19,22 @@ module.exports = {
        rpc: {
      host: 'localhost',
      post:8080
+=======
+      network_id: "1337" // Match any network id
+    }
+  /*  ropsten:  {
+         network_id: 3,
+         host: "localhost",
+         port:  8545,
+         from: "0xA12815106716D71c999fec63717d9A90B976ae75",
+         gas: 2900000,
+         gasPrice: 10000000000,
+
+    }
+      },
+       rpc: {
+     host: 'localhost',
+     post:8080*/
+>>>>>>> 5de84e81df7a6e2f8f781d4e7f0278f2ae301952
    }
     };
